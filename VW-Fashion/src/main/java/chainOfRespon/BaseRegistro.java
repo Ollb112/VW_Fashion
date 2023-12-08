ea23dd81f7301cf938e8dc13bc1471c9b511854d
DiegoMarkzs <119617704+DiegoMarkzs@users.noreply.github.com>
2023-12-08 00:37:57 -0300

    Delete VW-Fashion/src/main/java directory

--- VW-Fashion/src/main/java/chainOfRespon/BaseRegistro.java
+++
@@ -1,9 +0,0 @@
-package chainOfRespon;
-
-import model.User;
-
-public interface BaseRegistro {
-	
-	boolean processar(User usuario);
-
-}
--- VW-Fashion/src/main/java/proxySeg/LoginBase.java
+++
@@ -1,9 +0,0 @@
-package proxySeg;
-
-import model.User;
-
-public interface LoginBase {
-	
-	public User fazerLogin(String email, String senha);
-
-}
